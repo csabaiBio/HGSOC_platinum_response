eb0bec1c5c347d88f74a24b51353189223885ce5
Oz Kilim <ozkilim@hotmail.co.uk>
2024-01-24 20:52:45 +0100

    cleanup

--- Set-Tree/exps/eval_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .general import *
-from .metrics import *
-from .plotting import *
-from .train_utils import *
--- Set-Tree/settree/__init__.py
+++
@@ -1,8 +0,0 @@
-from settree.set_data import *
-from settree.set_tree import *
-from settree.gbest import *
-from settree.set_rf import *
-
-from settree.operations import *
-from settree.explainability import *
-
